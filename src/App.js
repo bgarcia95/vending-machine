283bd013fbaba8fd8cf3f87cbc49deaf8c893dc9
Brayan Garcia <bgarcia.95@outlook.com>
2021-01-22 16:13:44 -0600

    connect app with redux and style system

--- src/App.js
+++ src/App.js
@@ -1,21 +1,31 @@
-function App() {
+import { Provider } from "react-redux";
+import { ThemeProvider } from "styled-components";
+
+import configStore from "./redux/store/config";
+import theme from "./utils/theme";
+
+const App = () => {
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={configStore}>
+      <ThemeProvider theme={theme}>
+        <div className='App'>
+          <header className='App-header'>
+            <p>
+              Edit <code>src/App.js</code> and save to reload.
+            </p>
+            <a
+              className='App-link'
+              href='https://reactjs.org'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              Learn React
+            </a>
+          </header>
+        </div>
+      </ThemeProvider>
+    </Provider>
   );
-}
+};
 
 export default App;
