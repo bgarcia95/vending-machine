e3196f3a2fc41a2b4efecafba9dd6ec3b2856074
Brayan Garcia <bgarcia.95@outlook.com>
2021-01-22 15:38:08 -0600

    Add product class and create  dummy data

---
+++ src/data/data.js
@@ -0,0 +1,60 @@
+import Product from "../models/Product";
+
+export const DUMMY_DATA = [
+  new Product(
+    1,
+    "Ramen",
+    "https://s1.eestatic.com/2016/11/15/cocinillas/Cocinillas_170994703_116270237_1024x576.jpg",
+    3,
+    "60000"
+  ),
+  new Product(
+    2,
+    "Pizza",
+    "https://www.saborusa.com/wp-content/uploads/2019/10/Animate-a-disfrutar-una-deliciosa-pizza-de-salchicha-Foto-destacada.png",
+    1,
+    "50000"
+  ),
+  new Product(
+    3,
+    "Hamburger",
+    "//littlesunnykitchen.com/wp-content/uploads/2019/09/Air-fryer-hamburgers-6.jpg",
+    2,
+    "40000"
+  ),
+  new Product(
+    4,
+    "Fries",
+    "//img.apmcdn.org/4b2716626c9ff3f6e5dfebe520eb592c33cf1e7b/uncropped/b5464d-splendid-table-french-fries.jpg",
+    1.5,
+    "30000"
+  ),
+  new Product(
+    5,
+    "Cookies",
+    "//hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/delish-ms-fields-cookies-024-1544735513.jpg?resize=*",
+    0.5,
+    "20000"
+  ),
+  new Product(
+    6,
+    "Braided Bread",
+    "//lh3.googleusercontent.com/proxy/G-KBk3XCSIO0Kgb5EL-gt4fHEJmZkZ_WE_7EPtM02pck7LIFGE6U0SI-HV_woO0cjWUYaYJmE4XqK-IW37A80xcyUcLqCEaUWYHOziUI33C-nM69wIqrAPyZ",
+    0.25,
+    "10000"
+  ),
+  new Product(
+    7,
+    "Popcorn",
+    "//houseofnasheats.com/wp-content/uploads/2019/03/Stovetop-Popcorn-8.jpg",
+    0.25,
+    "10000"
+  ),
+  new Product(
+    8,
+    "Nuggets",
+    "//www.eatingbirdfood.com/wp-content/uploads/2020/01/chicken-nuggets-2.jpg",
+    0.25,
+    "10000"
+  ),
+];
---
+++ src/models/Product.js
@@ -0,0 +1,11 @@
+class Product {
+  constructor(id, name, urlImage, price, cookingTime) {
+    this.id = id;
+    this.name = name;
+    this.urlImage = urlImage;
+    this.price = price;
+    this.cookingTime = cookingTime;
+  }
+}
+
+export default Product;
